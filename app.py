37a9e928859d30c0087be26a15d9c8cd1b2d3ca8
dandeals <fosu6541@gmail.com>
2026-06-08 19:03:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -177,4 +177,6 @@ def payment_success_webhook():
         return jsonify({"success": False, "error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=False, port=5000)
+      import os
+      port = int(os.environ.get("PORT", 5000))
+      app.run(host='0.0.0.0', port=port)
